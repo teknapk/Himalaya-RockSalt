63fccc77b1dba9dc3af90da2185425856b1d1b58
Tekna PK <151453777+teknapk@users.noreply.github.com>
2026-08-14 17:06:12 +0500

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,6 @@
 <footer>
     <div class="container-fluid pt-2 p-3">
         <div class="row">
-            <!-- <div class="col-md-2 col-sm-12 col-xs-12 col-12">
-                <div class="footer-logo">
-                    <a href="#"><img class="logo" src="{{ asset('Assests/logo/picture9.png') }}" style="width:100%;"></a>
-                </div>
-            </div> -->
             <div class="col-md-1"></div>
             <div class="col-md-4 col-sm-12 col-xs-12 col-12">
                 <div class="for-about">
@@ -15,66 +10,58 @@
                         Himalayan region.</p>
                 </div>
                 <div class="footer-logo">
-                    <a href="#"><img class="logo" src="{{ asset('Assests/logo/picture9.png') }}"
-                            style="width:100%;"></a>
+                    <a href="{{ url('/') }}"><img class="logo" src="{{ asset('Assests/logo/picture9.png') }}" style="width:100%;" alt="Himalaya Rock Salt Logo"></a>
                 </div>
             </div>
             <div class="col-md-1"></div>
             <div class="col-md-5 col-sm-12 col-xs-12 col-12">
                 <div class="touch-links">
                     <p class="get-touch">CONNECT WITH US</p>
-                    <!-- <p class="font-14"><i class="fa fa-home pr-2"></i> #5, Safari Palace Avenue, Safari Villas 1, Bahria
-                        Town,
-                        Islamabad, Pakistan
-                    </p> -->
                     <p class="font-14"><i class="fa fa-phone pr-2"></i>
                         <button type="button" class="btn for_tooltip" data-toggle="tooltip" data-placement="top"
                             title="0319 8356275 ">
                             +92 319 TEKNAPK,
                         </button>
-                        +92 300 8569337, +92 332 8569337,+92 321 5169077
+                        +92 300 8569337, +92 332 8569337, +92 321 5169077
                     </p>
-                    <p class="font-14"><i class="fa fa-envelope pr-2"></i> info@teknapk.com, teknapk@icloud.com,
-                        bilal@teknapk.com
+                    <p class="font-14"><i class="fa fa-envelope pr-2"></i> info@teknapk.com, teknapk@icloud.com, bilal@teknapk.com
                     </p>
                     <p class="font-14">
-
-                        <!-- <i class="fa fa-globe pr-2"></i> -->
-
-                        https://teknapk.com/
+                        <i class="fa fa-globe pr-2"></i>
+                        <a href="https://teknapk.com/" class="text-dark">https://teknapk.com/</a>
                     </p>
                     <div class="social-links">
-
-                        <!-- <a href="#"><i class="fa fa-whatsapp text-info pr-2" aria-hidden="true"></i></a> -->
-                        <a href="https://www.facebook.com/HimalayaRockSalt/"> <i
-                                class="fa fa-facebook-square text-info pr-2"></i></a>
-                        <!-- <a href=""><i class="fa fa-whatsapp text-info pr-2"></i></a> -->
-                        <a href="https://wa.me/+923025306647" class="whatsapp_float" target="_blank"
-                            rel="noopener noreferrer">
-                            <i class="fa fa-whatsapp  text-info pr-2"></i>
+                        <a href="https://www.facebook.com/HimalayaRockSalt/" target="_blank" rel="noopener noreferrer">
+                            <i class="fa fa-facebook-square text-info pr-2"></i>
                         </a>
-                        <a href="http://twitter.com/teknapk"><i class="fa fa-twitter-square text-info pr-2"></i></a>
-                        <a href="https://instagram.com/himalayanpinksaltcrystal?igshid=OGQ5ZDc2ODk2ZA=="><i
-                                class="fa fa-instagram text-info pr-2"></i></a>
-                        <a href="https://www.linkedin.com/in/teknapk"><i
-                                class="fa fa-linkedin text-secondary text-info pr-2"></i></a>
-                        <a href="https://teknapk.com/"><i class="fa fa-globe text-info" aria-hidden="true"></i></a>
+                        <a href="https://wa.me/+923025306647" class="whatsapp_float" target="_blank" rel="noopener noreferrer">
+                            <i class="fa fa-whatsapp text-info pr-2"></i>
+                        </a>
+                        <a href="https://twitter.com/teknapk" target="_blank" rel="noopener noreferrer">
+                            <i class="fa fa-twitter-square text-info pr-2"></i>
+                        </a>
+                        <a href="https://instagram.com/himalayanpinksaltcrystal?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" rel="noopener noreferrer">
+                            <i class="fa fa-instagram text-info pr-2"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/teknapk" target="_blank" rel="noopener noreferrer">
+                            <i class="fa fa-linkedin text-info pr-2"></i>
+                        </a>
+                        <a href="https://teknapk.com/" target="_blank" rel="noopener noreferrer">
+                            <i class="fa fa-globe text-info" aria-hidden="true"></i>
+                        </a>
                     </div>
                     <hr>
                 </div>
-
             </div>
             <div class="col-md-1"></div>
-
         </div>
     </div>
 </footer>
-<div class="row w-100">
+<div class="row w-100 m-0">
     <div class="col-md-12">
         <div class="for_center text-center pt-2">
             <p class="copywrite_class">Copyright © 2004 Himalaya Rock Salt. Developed by App In Snap. All Rights
-                Reserved This website is Copyrighted. Copying contents or images without permission is prohibited.</p>
+                Reserved. This website is Copyrighted. Copying contents or images without permission is prohibited.</p>
         </div>
-
     </div>
 </div>
